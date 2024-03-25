860009af19ac8fa9495248f7292190aed4820b58
micagiaroli <161464885+micagiaroli@users.noreply.github.com>
2024-03-25 13:13:20 +0100

    Add files via upload

--- Giaroli. S2.01.sql
+++ Giaroli. S2.01.sql
@@ -7,6 +7,7 @@ WHERE company_id IN
     FROM company
     WHERE country = 'Germany');
     
+##CORREGIDO### 
 #N1.Exercici 2: llistat de les empreses que han realitzat transaccions per una suma superior a la mitjana de totes les transaccions.
 SELECT company_name 
 FROM company
@@ -14,88 +15,76 @@ WHERE company.id IN
 		(SELECT company_id
 		FROM transaction
 		GROUP BY company_id
-		HAVING sum(amount) >
+		HAVING AVG(amount) >
 			(SELECT AVG(amount) FROM transaction));
             
+            
+##CORREGIDO###            
 #N1.Exercici 3: nom d'empresa que inicia amb la lletra c, mostrar informació de les transaccions.
-SELECT company.company_name, transaction.*
-FROM transaction
-JOIN company ON transaction.company_id = company.id
-WHERE company.company_name LIKE 'C%';
-
-##alternativa con subquery y join
-SELECT company_name, transaction.*
-FROM transaction
-JOIN company ON transaction.company_id = company.id
-WHERE transaction.company_id IN
+SELECT t.*
+FROM transaction t
+WHERE t.company_id IN
 	(SELECT id
-    FROM company
+    FROM company c
     WHERE company_name LIKE 'C%');
+
     
+##CORREGIDO###  
 #N1.Exercici 4: Van eliminar del sistema les empreses que no tenen transaccions registrades, lliura el llistat d'aquestes empreses.
 SELECT company_name
 FROM company
-LEFT JOIN transaction ON transaction.company_id = company.id
-WHERE company_id IS NULL;
-
-##alternativa con subquery
-SELECT company_name
-FROM company
-WHERE id NOT IN 
+WHERE NOT EXISTS
 	(SELECT distinct company_id
     FROM transaction);
     
+##CORREGIDO###  
 ## NIVELL 2
 #N2.Exercici 1: totes les transaccions realitzades per empreses que estan situades en el mateix país que aquesta companyia 'Non Institute'
-SELECT company.company_name, transaction.*
-FROM transaction
-LEFT JOIN company ON transaction.company_id = company.id
-WHERE company_name <> 'Non Institute' AND country IN 
-		(SELECT country
+SELECT t.*
+FROM transaction t
+WHERE company_id IN  (
+	SELECT c.id
+    FROM company c
+    WHERE company_name <> 'Non Institute' AND country = ( 
+		SELECT country
 		FROM company
-		WHERE company_name = 'Non Institute');
+		WHERE company_name = 'Non Institute'
+        )
+	);
+
         
 #N2.Exercici 2: trobar l'empresa que ha realitzat la transacció de major suma en la base de dades.
-SELECT company.company_name
-FROM company
-WHERE company.id IN (
-	SELECT company_id
-	FROM transaction
-	WHERE amount = 
-		(SELECT max(amount)
-        FROM transaction)
+SELECT c.company_name
+FROM company c
+WHERE c.id IN (
+	SELECT t.company_id
+	FROM transaction t
+	WHERE t.amount = 
+		(SELECT max(t.amount)
+        FROM transaction t)
 	);
-    
+
+###Corregido###    
 ## NIVELL 3
 #N3.Exercici 1: necessiten el llistat dels països la mitjana de transaccions dels quals sigui superior a la mitjana general.
-SELECT country, AVG(amount) AS avg_by_country
-FROM company
-JOIN transaction
-ON company.id = transaction.company_id
+SELECT country, ROUND(AVG(amount), 2) AS avg_by_country
+FROM ( SELECT c.country, t.amount
+	FROM company c, transaction t
+    WHERE c.id=t.company_id) AS transactions_by_country
 GROUP BY country
-HAVING AVG(amount) > 
-	(SELECT AVG(amount)
-	FROM transaction);
-    
+HAVING AVG(amount) > (
+	SELECT AVG(amount)
+    FROM transaction);
+      
+
+###CORREGIDO       
 #N3.Exercici 2: llistat de les empreses on especifiquis si tenen més de 4 transaccions o menys.
-SELECT count(transaction.id) as number_trans, company_name,
+SELECT count(t.id), c.company_name,
 	CASE 	
-		WHEN count(transaction.id) > 4 THEN 'T' 
+		WHEN count(t.id) > 4 THEN 'T' 
         ELSE 'F'
         END AS more_than_4
-FROM transaction
-JOIN company
-ON transaction.company_id = company.id
-GROUP BY company_name;
-
-## alternativa con subquerys y usando IF
-
-SELECT company_name, number_trans,
-    IF(number_trans > 4, 'T', 'F') AS more_than_4
-FROM 
-    (SELECT COUNT(transaction.id) AS number_trans, company.company_name
-    FROM transaction
-    JOIN company
-    ON transaction.company_id = company.id
-    GROUP BY company.company_name) AS tab_num_tran_comp;
+FROM transaction t, company c
+WHERE t.company_id = c.id
+GROUP BY c.company_name;
 
